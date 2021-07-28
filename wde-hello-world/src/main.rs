e7fc065548c675603b53ab88c5994fae8ddf883d
rodz <rodrigo.f.rodrigues@hotmail.com>
2021-07-28 14:21:55 -0700

    feat(wde-hello-world): fix env var usage and update yaml

--- wde-hello-world/src/main.rs
+++ wde-hello-world/src/main.rs
@@ -3,10 +3,7 @@ pub const DEBUG_ECHO_DEVICE_NAME_LABEL: &str = "DEBUG_ECHO_DESCRIPTION";
 use std::env;
 
 fn main() {
-    let device_name = match env::var(DEBUG_ECHO_DEVICE_NAME_LABEL) {
-        Ok(n) => n,
-        Err(_e) => "debugEcho".to_string(),
-    };
+    let device_name = env::var(DEBUG_ECHO_DEVICE_NAME_LABEL).unwrap();
     println!(
         "Pod is running and using debugEcho device named: {}",
         device_name
